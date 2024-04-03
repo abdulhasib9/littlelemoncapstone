1ea457d75f87b1d7e2a51f0632b71ba51288290a
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-04-03 11:23:20 -0700

    added rest framework and serializers class" ;

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'restaurant.apps.RestaurantConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
