273d4e2622b0eab3df6be52d187133a35b251563
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-04-03 18:21:56 -0700

    added userserializer

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from . import models
+from django.contrib.auth.models import User
 
 
 class MenuSerializer(serializers.ModelSerializer):
@@ -11,3 +12,8 @@ class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Booking
         fields = '__all__'
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url','username','email','groups']
