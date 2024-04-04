886cf1cce9ee377407c31e21166094ad48f7bd9b
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-04-04 06:15:20 -0700

    added some test assertions

--- restaurant/tests/test-models.py
+++ restaurant/tests/test-models.py
@@ -4,15 +4,15 @@ from restaurant.models import Menu
 class MenuModelTest(TestCase):
     def test_create_menu(self):
         # create a new menu object
-        menu = Menu.objects.create(title='Lunch', price=20, inventory=4)
+        menu = Menu.objects.create(name='Lunch', price=20, description='A selection of Lunch items')
 
         # check that the string representation of the object is as expected
         self.assertEqual(str(menu), 'Lunch : A selection of Lunch items')
 
         # check that the name, price, and description fields are set correctly
-        self.assertEqual(menu.title, 'Lunch')
+        self.assertEqual(menu.name, 'Lunch')
         self.assertEqual(menu.price, 20)
-        self.assertEqual(menu.inventory, 4)
+        self.assertEqual(menu.description, 'A selection of Lunch items')
 
         # check that the calculate_total_cost method works correctly
         self.assertEqual(menu.calculate_total_cost(), 30)
