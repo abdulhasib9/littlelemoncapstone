97be3fa1f115ae4f2b59e3f43b1e98056d119cb0
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-04-03 18:53:47 -0700

    added djoser to the project

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'restaurant.apps.RestaurantConfig',
     'rest_framework',
     'rest_framework.authtoken',
+    'djoser',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -135,3 +136,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJOSER={"USER_ID_FIELD":"username"}
